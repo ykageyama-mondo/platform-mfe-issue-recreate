3f869474657da16be183eb92aefaec7c03c36ccf
Yuichi <yuichi.kageyama@mondo.com.au>
2025-06-05 16:22:06 +1000

    inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Platform MFE load issue recreation
+
+Repo to automate recreation of the platform MFE load race condition
---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import { chromium } from 'playwright';
+import 'dotenv/config'
+
+let iterations = 0;
+
+const run = async () => {
+  iterations++;
+  const browser = await chromium.launch({
+    headless: false, // Set to false if you want to see the browser
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  let step = 'Loading page'
+
+  page.on('console', (msg) => {
+    if (msg.type() !==  'error') {
+      return;
+    }
+
+    if (msg.text().includes('Minified React error')) {
+      console.error(`Error in iteration ${iterations}. Failed at step ${step}:\n`, msg.text());
+      process.exit(1);
+    }
+  })
+
+  await page.goto(process.env.URL!);
+
+
+  step = 'Logging in';
+
+  await page.getByPlaceholder('Email').fill(process.env.EMAIL!)
+  await page.getByPlaceholder('Password').fill(process.env.PASSWORD!)
+
+  await page.locator('button[type="submit"]', {hasText: 'Sign in'}).click()
+
+  step = 'Loading dashboard';
+
+
+  await page.waitForLoadState('networkidle');
+
+  step = 'Navigating to Analysis';
+
+  await page.locator('a', {hasText: 'Analysis'}).click()
+
+  step = 'Loading analysis page';
+
+  await page.waitForURL('**/analysis');
+
+  await page.waitForLoadState('networkidle');
+
+  await page.waitForTimeout(2_000);
+
+  page.removeAllListeners();
+
+  await browser.close();
+
+  console.log('Logged in successfully!');
+}
+
+const main = async () => {
+
+  while(true) {
+    await run()
+  }
+
+}
+main()
